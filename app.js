d1a4c7b55e907d7e256bce5eb02524e5da3c06f5
Walidnurudin <walidnurudin47@gmail.com>
2021-01-28 14:34:19 +0700

    alert in Category, set title headers

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,6 +26,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+}));
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,50 +2,76 @@ const Category = require('../models/Category');
 
 module.exports = {
     viewDashboard: (req, res) => {
-        res.render('admin/dashboard/view_dashboard.ejs')
+        res.render('admin/dashboard/view_dashboard.ejs', { title: 'Staycation | Dashboard' })
     },
 
     viewCategory: async (req, res) => {
-        const category = await Category.find();
-        // console.log({ category })
-        res.render('admin/category/view_category.ejs', { category })
+        try {
+            const alertMessage = req.flash('alertMessage');
+            const alertStatus = req.flash('alertStatus');
+            const alert = { message: alertMessage, status: alertStatus }
+            const category = await Category.find();
+            res.render('admin/category/view_category.ejs', { category, alert, title: 'Staycation | Category' })
+        } catch (error) {
+            res.redirect('/admin/category');
+        }
+
     },
 
     addCategory: async (req, res) => {
         try {
             const { name } = req.body;
             await Category.create({ name });
+            req.flash('alertMessage', 'Success Add Category');
+            req.flash('alertStatus', 'success');
             res.redirect('/admin/category');
         } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
             res.redirect('/admin/category');
-
         }
     },
 
     editCategory: async (req, res) => {
-        const { id, name } = req.body;
-        const category = await Category.findOne({ _id: id });
-        category.name = name;
-        await category.save();
-        res.redirect('/admin/category')
+        try {
+            const { id, name } = req.body;
+            const category = await Category.findOne({ _id: id });
+            category.name = name;
+            await category.save();
+            req.flash('alertMessage', 'Success Update Category');
+            req.flash('alertStatus', 'success');
+            res.redirect('/admin/category')
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/category');
+        }
     },
 
     deleteCategory: async (req, res) => {
-        const { id } = req.params;
-        const category = await Category.findOne({ _id: id });
-        await category.remove();
-        res.redirect('/admin/category');
+        try {
+            const { id } = req.params;
+            const category = await Category.findOne({ _id: id });
+            await category.remove();
+            req.flash('alertMessage', 'Success Delete Category');
+            req.flash('alertStatus', 'success');
+            res.redirect('/admin/category');
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/category');
+        }
     },
 
     viewBank: (req, res) => {
-        res.render('admin/bank/view_bank.ejs')
+        res.render('admin/bank/view_bank.ejs', { title: 'Staycation | Bank' })
     },
 
     viewItem: (req, res) => {
-        res.render('admin/item/view_item.ejs')
+        res.render('admin/item/view_item.ejs', { title: 'Staycation | Item' })
     },
 
     viewBooking: (req, res) => {
-        res.render('admin/booking/view_booking.ejs')
+        res.render('admin/booking/view_booking.ejs', { title: 'Staycation | Booking' })
     }
 }
