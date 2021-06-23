60456cfc6228e4209a6fe9d4810842d6062ce906
Walidnurudin <walidnurudin47@gmail.com>
2021-06-24 06:21:27 +0700

    booking view

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,8 @@ const Item = require('../models/Item');
 const Image = require('../models/Image');
 const Feature = require('../models/Feature');
 const Activity = require('../models/Activity');
+const Booking = require('../models/Booking');
+const Member = require('../models/Member');
 const Users = require('../models/Users');
 const fs = require('fs-extra');
 const path = require('path');
@@ -67,7 +69,7 @@ module.exports = {
                 user: req.session.user,
             })
         } catch (error) {
-            
+
         }
     },
 
@@ -574,10 +576,20 @@ module.exports = {
     },
 
     // BOOKING
-    viewBooking: (req, res) => {
-        res.render('admin/booking/view_booking.ejs', {
-            title: 'Staycation | Booking',
-            user: req.session.user,
-        })
+    viewBooking: async (req, res) => {
+        try {
+            const booking = await Booking.find()
+            .populate('bankId')
+            .populate('memberId');
+            res.render('admin/booking/view_booking.ejs', {
+                title: 'Staycation | Booking',
+                user: req.session.user,
+                booking,
+            })
+        } catch (error) {
+            // req.flash('alertMessage', `${error.message}`);
+            // req.flash('alertStatus', 'danger');
+            // res.redirect(`/admin/booking`);
+        }
     }
 }
