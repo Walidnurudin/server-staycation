92e63e00aa28c52615935908e83c497009ce75a4
Walidnurudin <walidnurudin47@gmail.com>
2021-06-24 02:05:53 +0700

    auth

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -15,14 +15,19 @@ module.exports = {
             const alertMessage = req.flash('alertMessage');
             const alertStatus = req.flash('alertStatus');
             const alert = { message: alertMessage, status: alertStatus };
-            res.render('index', {
-                alert,
-                title: 'Staycation | Login',
-            })
-        } catch (error) {
+            if(req.session.user == null || req.session.user == undefined){
+                res.render('index', {
+                    alert,
+                    title: 'Staycation | Login',
+                })    
+            }else{
+                res.redirect('/admin/dashboard')
+            }
+            } catch (error) {
             res.redirect('/admin/signin')
         }
     },
+
     actionSignin: async (req, res) => {
         try {
             const { username, password } = req.body;
@@ -32,18 +37,24 @@ module.exports = {
                 req.flash('alertStatus', 'danger');
                 res.redirect('/admin/signin');
             }
+
             const isPasswordMatch = await bcrypt.compare(password, user.password);
             if(!isPasswordMatch){
                 req.flash('alertMessage', 'Password yang anda masukan tidak cocok!');
                 req.flash('alertStatus', 'danger');
                 res.redirect('/admin/signin');
             }
-
+            
+            req.session.user = {
+                id: user.id,
+                username: user.username,
+            }
             res.redirect('/admin/dashboard');
         } catch (error) {
             res.redirect('/admin/signin');
         }
     },
+
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard.ejs', { title: 'Staycation | Dashboard' })
     },
