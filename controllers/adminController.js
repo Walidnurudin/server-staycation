580a654d9e88ebe74b4ed2c09b45eac7ec8fc845
Walidnurudin <walidnurudin47@gmail.com>
2021-06-28 02:56:09 +0700

    action confirm & reject booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -592,19 +592,52 @@ module.exports = {
     },
 
     showDetailBooking: async (req, res) => {
+        const { id } = req.params;
         try {
-            const { id } = req.params;
+            const alertMessage = req.flash('alertMessage');
+            const alertStatus = req.flash('alertStatus');
+            const alert = { message: alertMessage, status: alertStatus };
+
             const booking = await Booking.findOne({ _id: id })
                 .populate('bankId')
                 .populate('memberId');
-                console.log(booking)
+            console.log(booking)
             res.render('admin/booking/show_detail_booking', {
                 title: 'Staycation | Detail Booking',
                 user: req.session.user,
                 booking,
+                alert,
             })
         } catch (error) {
+            res.redirect(`/admin/booking`);
+        }
+    },
 
+    actionConfirmation: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const booking = await Booking.findOne({ _id: id });
+            booking.payments.status = 'Accept';
+            await booking.save();
+            req.flash('alertMessage', 'Success Confirmation Pembayaran');
+            req.flash('alertStatus', 'success');
+            res.redirect(`/admin/booking/${id}`);
+        } catch (error) {
+            res.redirect(`/admin/booking/${id}`);
+        }
+    },
+
+    actionReject: async (req, res) => {
+        const {id} = req.params;
+        try {
+            const booking = await Booking.findOne({_id: id});
+            booking.payments.status = "Reject";
+            await booking.save();
+            req.flash('alertMessage', 'Success Reject Pembayaran');
+            req.flash('alertStatus', 'success');
+            res.redirect(`/admin/booking/${id}`);
+        }catch(error){
+            res.redirect(`/admin/booking/${id}`)
         }
     }
 }
