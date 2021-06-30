922ea372020f33d59c584adc4d74d007d4129ae1
Walidnurudin <walidnurudin47@gmail.com>
2021-06-30 09:43:47 +0700

    API detail page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Treveler = require('../models/Booking');
 const Category = require('../models/Category');
+const Bank = require('../models/Bank');
 
 module.exports = {
     landingPage: async (req, res) => {
@@ -18,7 +19,7 @@ module.exports = {
                     path: 'itemId',
                     select: '_id title country city isPopular imageId',
                     perDocumentLimit: 4,
-                    option: {sort: {sumBooking: -1}},
+                    option: { sort: { sumBooking: -1 } },
                     populate: {
                         path: 'imageId',
                         select: '_id imageUrl',
@@ -32,15 +33,25 @@ module.exports = {
 
             for (let i = 0; i < category.length; i++) {
                 for (let x = 0; x < category[i].itemId.length; x++) {
-                    const item = await Item.findOne({_id: category[i].itemId[x]._id});
+                    const item = await Item.findOne({ _id: category[i].itemId[x]._id });
                     item.isPopular = false;
                     await item.save();
-                    if(category[i].itemId[0] === category[i].itemId[x]){
+                    if (category[i].itemId[0] === category[i].itemId[x]) {
                         item.isPopular = true;
                         await item.save();
                     }
                 }
             }
+
+            const testimonial = {
+                _id: "asd1293uasdads1",
+                imageUrl: "/images/testimonial-landingpages.jpg",
+                name: "Happy Family",
+                rate: 4.55,
+                content: "What a great trip with my family and I should try again next time soon ...",
+                familyName: "Angga",
+                familyOccupation: "Product Designer",
+              }
 
             res.status(200).json({
                 hero: {
@@ -50,10 +61,41 @@ module.exports = {
                 },
                 mostPicked,
                 category,
+                testimonial,
             });
         } catch (error) {
             console.log(error);
-            rea.status(500).json({message: "Internal server error"});
+            rea.status(500).json({ message: "Internal server error" });
+        }
+    },
+
+    detailPage: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const item = await Item.findOne({ _id: id })
+                .populate({ path: 'featureId', select: '_id name qty imageUrl' })
+                .populate({ path: 'activityId', select: '_id name type imageUrl' })
+                .populate({ path: 'imageId', select: '_id imageUrl' });
+
+            const bank = await Bank.find();
+
+            const testimonial = {
+                _id: "asd1293uasdads1",
+                imageUrl: "/images/testimonial-detailspage.jpg",
+                name: "Happy Family",
+                rate: 4.25,
+                content: "What a great trip with my family and I should try again and again next time soon...",
+                familyName: "Angga",
+                familyOccupation: "UI Designer",
+            }
+
+            res.status(200).json({
+                ...item._doc,
+                bank,
+                testimonial,
+            })
+        } catch (error) {
+
         }
     }
 }
