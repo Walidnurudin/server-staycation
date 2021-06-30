b38c2a7934989c17f03e7ae34c41af4e8ed9addc
Walidnurudin <walidnurudin47@gmail.com>
2021-06-30 09:19:08 +0700

    API landing page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,59 @@
+const Item = require('../models/Item');
+const Treasure = require('../models/Activity');
+const Treveler = require('../models/Booking');
+const Category = require('../models/Category');
+
 module.exports = {
     landingPage: async (req, res) => {
-        const message = 'Hello json';
-        res.status(200).json({message});
+        try {
+            const mostPicked = await Item.find()
+                .select('_id title country city price unit imageId')
+                .limit(5)
+                .populate({ path: 'imageId', select: '_id imageUrl' });
+
+            const category = await Category.find()
+                .select('_id name')
+                .limit(3)
+                .populate({
+                    path: 'itemId',
+                    select: '_id title country city isPopular imageId',
+                    perDocumentLimit: 4,
+                    option: {sort: {sumBooking: -1}},
+                    populate: {
+                        path: 'imageId',
+                        select: '_id imageUrl',
+                        perDocumentLimit: 1,
+                    },
+                })
+
+            const treveler = await Treveler.find();
+            const treasure = await Treasure.find();
+            const city = await Item.find();
+
+            for (let i = 0; i < category.length; i++) {
+                for (let x = 0; x < category[i].itemId.length; x++) {
+                    const item = await Item.findOne({_id: category[i].itemId[x]._id});
+                    item.isPopular = false;
+                    await item.save();
+                    if(category[i].itemId[0] === category[i].itemId[x]){
+                        item.isPopular = true;
+                        await item.save();
+                    }
+                }
+            }
+
+            res.status(200).json({
+                hero: {
+                    trevelers: treveler.length,
+                    treasures: treasure.length,
+                    cities: city.length,
+                },
+                mostPicked,
+                category,
+            });
+        } catch (error) {
+            console.log(error);
+            rea.status(500).json({message: "Internal server error"});
+        }
     }
 }
